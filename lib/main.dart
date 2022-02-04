411d17d3a3b4ee300e144eac26154c147b137a27
Glênio Carvalho <gleniocarvalho@MacBook-Pro-de-Glenio.local>
2022-02-04 00:56:15 -0300

    feat: first commit

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: const Text('Olá mundão'),
+      ),
+    );
+  }
+}
