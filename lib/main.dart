1560addfd1b516d9fbae9b618a9ac44439ca9cd6
Glênio Carvalho <gleniocarvalho@MacBook-Pro-de-Glenio.local>
2022-02-05 13:39:14 -0300

    feat: add background image

--- lib/main.dart
+++ lib/main.dart
@@ -9,30 +9,79 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: HomePage());
+    return const MaterialApp(
+      home: HomePage(),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  void decrement() {
+    print('Decrement');
+  }
+
+  void increment() {
+    print('Increment');
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: const [
-          Text(
-            'Pode entrar!',
-            style: TextStyle(
-                fontSize: 26, color: Colors.white, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            '11',
-            style: TextStyle(color: Colors.white, fontSize: 40),
-          ),
-        ],
+      backgroundColor: Colors.red,
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+                image: AssetImage('assets/images/background.png'),
+                fit: BoxFit.cover)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Pode entrar caraio!',
+              style: TextStyle(
+                  fontSize: 26,
+                  color: Colors.black,
+                  fontWeight: FontWeight.bold),
+            ),
+            const Padding(
+              padding: EdgeInsets.all(32),
+              child: Text(
+                '0',
+                style: TextStyle(color: Colors.black, fontSize: 40),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextButton(
+                  onPressed: decrement,
+                  style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      primary: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(16)),
+                      fixedSize: const Size(100, 100)),
+                  child: const Text('Saiu',
+                      style: TextStyle(color: Colors.black, fontSize: 18)),
+                ),
+                const SizedBox(width: 32),
+                TextButton(
+                    onPressed: increment,
+                    style: TextButton.styleFrom(
+                        backgroundColor: Colors.white,
+                        primary: Colors.black,
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16)),
+                        fixedSize: const Size(100, 100)),
+                    child: const Text('Entrou',
+                        style: TextStyle(color: Colors.black, fontSize: 18))),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
